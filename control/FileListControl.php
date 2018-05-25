236a643c1d88280b745a9939f4619465a1cdaf1e
Henning Janßen <development@henning-janssen.net>
2018-05-25 17:33:02 +0200

    Delete files

--- control/FileListControl.php
+++ control/FileListControl.php
@@ -2,14 +2,32 @@
 namespace axl\fstore\control;
 
 use axolotl\control\LoggedInPageControl;
+use axolotl\util\_;
+use axolotl\util\Doctrine;
+use axolotl\view\RedirectView;
 
 use axl\fstore\model\File;
 use axl\fstore\view\FileListView;
 
 class FileListControl extends LoggedInPageControl{
   public function execute(): void{
+    if(_::GET('delete', -1) > 0){
+      $del = File::get(intval(_::GET('delete')));
+      if($del){
+        $em = Doctrine::getEntityManager();
+        $em->remove($del);
+        $em->flush();
+      }
+      $link = '/m/AXL/fstore/files';
+      if($this->hasVar('group')){
+        $link .= '/'.$this->vars['group'];
+      }
+      (new RedirectView($link))->render();
+      return;
+    }
+
     $group = null;
-    if(array_key_exists('group', $this->vars)){
+    if($this->hasVar('group')){
       $group = $this->vars['group'];
     }
     $files = File::getAll($group);
